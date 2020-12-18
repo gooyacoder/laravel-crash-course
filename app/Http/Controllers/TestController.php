849e0a5d93e881be9d1b885a255587b47b325781
amirhosein <gooyacoder@gmail.com>
2020-12-18 22:03:40 +0330

    Upto 05_07

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Page;
 
 class TestController extends Controller
 {
     public function test1(){
-    	echo 'Controller function worked successfully.';
+    	$pages = Page::all();
+    	foreach ($pages as $page) {
+    		echo "$page->name / $page->description <br>";
+    	}
+
     }
 }
