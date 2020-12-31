dd2b4bf519911c78fe6608628e5fe1d2d3e7c913
amirhosein <gooyacoder@gmail.com>
2020-12-31 18:18:42 +0330

    Upto 06_01

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,10 +8,8 @@ use App\Models\Page;
 class TestController extends Controller
 {
     public function test1(){
-    	$pages = Page::all();
-    	foreach ($pages as $page) {
-    		echo "$page->name / $page->description <br>";
-    	}
+    	$pages = Page::whereId(2)->first();
+    	return view('website.test1', ['pages' => $pages]);
 
     }
 }
---
+++ resources/views/website/test1.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test Page 1</title>
+</head>
+<body>
+
+	<p>{{ $pages->name }}</p>
+	<p>{{ $pages->description }}</p>
+
+</body>
+</html>
